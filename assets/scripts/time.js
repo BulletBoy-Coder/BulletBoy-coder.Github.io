03c4af1bc05e2fb4c57c34573ae487f81946f9e0
userandaname <zayaan.rajput@gmail.com>
2024-09-25 19:54:16 +0100

    Update time details

--- assets/scripts/time.js
+++ assets/scripts/time.js
@@ -1,6 +1,6 @@
 function showTime() {
 	const now = new Date();
-	document.getElementById('currentTime').innerHTML = now.toLocaleString('en-GB', { timeZone: 'Europe/London', hour: '2-digit', minute: '2-digit', hour12: false });
+	document.getElementById('currentTime').innerHTML = now.toLocaleString('en-GB', { timeZone: 'Europe/London' });
 }
 
 showTime();
